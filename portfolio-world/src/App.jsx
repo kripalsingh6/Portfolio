eb5cfbe092f1a1ad3f5d1189039d8a905659b3a7
kripal <thakurkripalsingh6@gmail.com>
2026-04-05 22:27:19 +0530

    create jsx file

--- portfolio-world/src/App.jsx
+++ portfolio-world/src/App.jsx
@@ -1,10 +1,20 @@
+import { BrowserRouter } from "react-router-dom";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
-import './output.css'
-
-function App() {
+const App = ()=>{
   return (
-   <div class="text-3xl font-bold underline">hi welocome to my world</div>
+    <BrowserRouter>
+        <div className="relative z-0 bg-primary">
+           <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+            <Navbar/>
+            <Hero/>
+
+    </div>
+   </div>
+    </BrowserRouter>
+  
   )
 }
 
-export default App
+export default App;
