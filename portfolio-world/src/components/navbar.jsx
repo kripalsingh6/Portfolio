a14d492ac0276c48382bb16aaa5cc808aaf66a7d
kripal <thakurkripalsingh6@gmail.com>
2026-04-07 15:25:52 +0530

    navbar

--- portfolio-world/src/components/navbar.jsx
+++ portfolio-world/src/components/navbar.jsx
@@ -1,8 +1,20 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import {styles} from "../style";
+import { navLinks } from "../constants";
+import {logo , menu , close} from "../assets"
 
 export default function Navbar(){
+
     return(
-        <div>
-            Navbar
-        </div>
+        <nav className={` ${styles.paddingX} w-full flex 
+        items-center py-5 top-0 z-20
+         bg-primary`}>
+            <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+                hello
+            </div>
+
+        </nav>
     )
 }
