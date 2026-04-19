9e27c126c678e0135c8cf4084bb803648545926f
kripal <thakurkripalsingh6@gmail.com>
2026-04-19 20:14:41 +0530

    add file

--- portfolio-world/src/components/canvas/computers.jsx
+++ portfolio-world/src/components/canvas/computers.jsx
@@ -3,32 +3,34 @@ import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-import CanvasLoader from "../Loader";
+import CanvasLoader from "./Loader.jsx";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF("/desktop_pc/scene.gltf");
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
+      <hemisphereLight intensity={0.5} groundColor="black" />
+      <pointLight intensity={1.5} />
       <spotLight
         position={[-20, 50, 10]}
-        angle={0.12}
+        angle={0.2}
         penumbra={1}
         intensity={1}
         castShadow
-        shadow-mapSize={1024}
       />
-      <pointLight intensity={1} />
+
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        scale={isMobile ? 0.7 : 0.8}
+        position={isMobile ? [0, -3, -2] : [0, -3.2, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   );
 };
+
+useGLTF.preload("/desktop_pc/scene.gltf");
 
 const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
@@ -76,4 +78,4 @@ const ComputersCanvas = () => {
   );
 };
 
-export default ComputersCanvas;
+export { ComputersCanvas };
