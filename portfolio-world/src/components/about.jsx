cbcd7840b4632149076d8b3cfc30dd46a81d7208
kripal <thakurkripalsingh6@gmail.com>
2026-04-20 20:40:24 +0530

    create 3d page

--- portfolio-world/src/components/about.jsx
+++ portfolio-world/src/components/about.jsx
@@ -1,7 +1,42 @@
+import React from 'react';
+// import Tilt from 'react-tilt';
+import { motion } from 'framer-motion';
+
+import styles from '../style.js';
+import {services} from '../constants';
+import {fadeIn , textVariant} from '../utils/motion';
+
 export default function About(){
     return(
-        <div>
-            About
-        </div>
+       <>
+       <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>
+            Introduction
+        </p>
+        <h2 className={styles.sectionHeadText}>
+            Overview.
+        </h2>
+
+       </motion.div>
+
+       <motion.p 
+       variants={fadeIn("","",0.1,1)}
+       className='mt-4 text-[#aaa6c3] text-[17px] max-w-3xl leading-[30px]'>
+    Hi, I’m a software developer skilled in JavaScript, 
+    React, Node.js, and Express. 
+    I build scalable, user-friendly applications and enjoy turning ideas into real-world solutions.
+     Let’s bring your ideas to life.
+
+
+       </motion.p>
+
+       <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service , index)=>(
+            <serviceCard key={service.title} 
+            index={index} {...service}/>
+        ))}
+
+       </div>
+       </>
     )
 }
--- portfolio-world/src/components/hero.jsx
+++ portfolio-world/src/components/hero.jsx
@@ -1,6 +1,7 @@
 import {motion} from 'framer-motion';
 import styles from "../style.js";
  import {ComputersCanvas} from './canvas/computers.jsx';
+import { repeat } from 'maath/misc';
 
 export default function Hero(){
     return(
@@ -33,6 +34,32 @@ export default function Hero(){
           
         </div>
           <ComputersCanvas/>
+
+          <div className='absolute xs:bottom-10
+           bottom-32 w-full flex justify-center 
+           items-center'>
+            <a href='#about'>
+              <div className='w-[35px] h-[64px] 
+              rounded-3xl border-4 border-[#aaa6c3] 
+              flex justify-center items-start p-2 '>
+                <motion.div animate={{
+                  y:[0,24,0]
+                }}
+                transition={{
+                  duration:1.5,
+                  repeat: Infinity,
+                  repeatType: 'loop'
+                }}
+                className="w-3 h-3 rounded-full bg-[#aaa6c3]
+                mb-1"
+                >
+
+                </motion.div>
+
+              </div>
+            </a>
+
+          </div>
       </section>
     )
 }
